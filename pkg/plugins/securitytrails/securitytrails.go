8cd17dde25b326f6cc1e6a599af2052c95a0a8d7
Tim <11543666+tbckr@users.noreply.github.com>
2024-12-08 11:52:43 +0100

    chore: init securitytrails plugin

---
+++ pkg/plugins/securitytrails/securitytrails.go
@@ -0,0 +1,120 @@
+package securitytrails
+
+import (
+	"context"
+	"fmt"
+	"github.com/imroc/req/v3"
+	"log/slog"
+)
+
+const (
+	source = "securitytrails"
+
+	baseURL = "https://api.securitytrails.com/v1"
+)
+
+type SecurityTrailsClient struct {
+	*req.Client
+}
+
+type MetaResponse struct {
+	LimitReached bool `json:"limit_reached"`
+}
+
+type SubdomainResponse struct {
+	Endpoint       string       `json:"endpoint"`
+	Meta           MetaResponse `json:"meta"`
+	SubdomainCount int          `json:"subdomain_count"`
+	Subdomains     []string     `json:"subdomains"`
+}
+
+type DomainDetailsResponse struct {
+	AlexaRank      int         `json:"alexa_rank"`
+	ApexDomain     string      `json:"apex_domain"`
+	CurrentDNS     DNSResponse `json:"current_dns"`
+	Endpoint       string      `json:"endpoint"`
+	Hostname       string      `json:"hostname"`
+	SubdomainCount int         `json:"subdomain_count"`
+}
+
+type DNSResponse struct {
+	A    DNSRecordResponse `json:"a"`
+	AAAA DNSRecordResponse `json:"aaaa"`
+	MX   DNSRecordResponse `json:"mx"`
+	NS   DNSRecordResponse `json:"ns"`
+	SOA  DNSRecordResponse `json:"soa"`
+	TXT  DNSRecordResponse `json:"txt"`
+}
+
+type DNSRecordResponse struct {
+	FirstSeen string                   `json:"first_seen"`
+	Values    []map[string]interface{} `json:"values"`
+}
+
+func NewSecurityTrailsClient(reqClient *req.Client, apiKey string) *SecurityTrailsClient {
+	c := reqClient.Clone().
+		SetBaseURL(baseURL).
+		SetCommonContentType("application/json").
+		SetCommonHeader("APIKEY", apiKey)
+	return &SecurityTrailsClient{
+		Client: c,
+	}
+}
+
+func (c *SecurityTrailsClient) Ping() (bool, error) {
+	resp, err := c.R().
+		Get("/ping")
+	if err != nil {
+		return false, err
+	}
+	return resp.StatusCode == 200, nil
+}
+
+func (c *SecurityTrailsClient) Subdomains(ctx context.Context, domain string, subdomainsOnly, includeInactive bool) (resp SubdomainResponse, err error) {
+	var r *req.Response
+	r, err = c.R().
+		SetContext(ctx).
+		SetPathParam("hostname", domain).
+		SetQueryParam("children_only", fmt.Sprintf("%t", subdomainsOnly)).
+		SetQueryParam("include_inactive", fmt.Sprintf("%t", includeInactive)).
+		SetSuccessResult(&resp).
+		Get("/domain/{hostname}/subdomains")
+	if err != nil {
+		return
+	}
+	defer r.Body.Close()
+	return
+}
+
+func (c *SecurityTrailsClient) DomainDetails(ctx context.Context, domain string) (resp DomainDetailsResponse, err error) {
+	var r *req.Response
+	r, err = c.R().
+		SetContext(ctx).
+		SetPathParam("hostname", domain).
+		SetSuccessResult(&resp).
+		Get("/domain/{hostname}")
+	if err != nil {
+		return
+	}
+	defer r.Body.Close()
+	return
+}
+
+func (c *SecurityTrailsClient) FetchDomains(ctx context.Context, domain string) ([]string, error) {
+	slog.Debug("Fetching domains",
+		"source", source,
+		"domain", domain,
+	)
+
+	resp, err := c.Subdomains(ctx, domain, false, true) // TODO make this configurable via parameters
+	if err != nil {
+		return nil, err
+	}
+
+	slog.Debug("Retrieved domains",
+		"source", source,
+		"domain", domain,
+		"count", resp.SubdomainCount,
+	)
+	return resp.Subdomains, nil
+}
